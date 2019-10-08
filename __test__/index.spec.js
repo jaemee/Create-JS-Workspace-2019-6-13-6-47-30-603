a6efc2be3a5492a6baf9831da12d435776e8e164
JAE MELANEE BUGARIN (SHP-IRIS-ISD-OOCLL/MNL) <BUGARJA@OOCL.COM>
2019-10-08 17:11:42 +0800

    removed unnecessary files

--- __test__/index.spec.js
+++
@@ -1,5 +0,0 @@
-const add = require('../main');
-
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-});
--- main.js
+++
@@ -1,5 +0,0 @@
-function add (left, right) {
-    return left + right;
-}
-
-module.exports = add;
